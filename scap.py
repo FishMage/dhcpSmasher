1fcfe048d95a16c7585c64a75fed20a40ef2b036
FishMage <schen358@wisc.edu>
2018-04-13 13:17:29 -0400

    Display DHCP info

--- scap.py
+++ scap.py
@@ -2,11 +2,22 @@ from scapy.all import *
 import sys
 class sniffer():
     def dhcp_monitor(self,pkt):
+        
+        if pkt[DHCP].options[0][1]==1:
+          print "DHCP Discover"
+        if pkt[DHCP].options[0][1]==2:
+          print "DHCP Offer"
+        if pkt[DHCP].options[0][1]==3:
+          print "DHCP Request"
+        if pkt[DHCP].options[0][1]==4:
+          print "DHCP Decline"
+        if pkt[DHCP].options[0][1]==5:
+          print "ACK Received"
+        if pkt[DHCP].options[0][1]==6:
+            print "NAK Received"
         if pkt[DHCP]:
-            print str(pkt[IP].dst)+" dst"
-
-        elif pkt[DHCP].options[0][1]==6:
-            print "NAK received"
+            print "src: "+str(pkt[IP].src)+"--> dst: " + str(pkt[IP].dst)
+            
 
     def listen(self):
         sniff(filter="udp and (port 67 or port 68)",prn=self.dhcp_monitor,store=0)
